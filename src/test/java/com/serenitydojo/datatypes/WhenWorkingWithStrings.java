44f27528efe440691806d73a7210277950157fe6
branislav-besevic <branislav.besevic@gmail.com>
2023-04-29 22:05:24 +0200

    Farenheit
    I had some problems with farenheit conversion. Float was giving number 80.599... and using (9/5) was not working as intended, had to convert it to 1.8.

--- src/test/java/com/serenitydojo/datatypes/WhenWorkingWithStrings.java
+++ src/test/java/com/serenitydojo/datatypes/WhenWorkingWithStrings.java
@@ -2,6 +2,7 @@ package com.serenitydojo.datatypes;
 
 import org.junit.Test;
 
+
 import static org.hamcrest.CoreMatchers.equalTo;
 import static org.hamcrest.MatcherAssert.assertThat;
 
@@ -11,9 +12,9 @@ public class WhenWorkingWithStrings {
     public void convertToLowerCase() {
         String bookTitle = "The Cat In The Hat";
 
-        String lowerCaseTitle = "";
+        String lowerCaseTitle = bookTitle.toLowerCase();
         // TODO: Convert the book title to lower case and assign it to the lowerCaseTitle variable
-
+        System.out.println(lowerCaseTitle);
         assertThat(lowerCaseTitle, equalTo("the cat in the hat"));
 
     }
@@ -22,9 +23,9 @@ public class WhenWorkingWithStrings {
     public void convertToUpperCase() {
         String bookTitle = "The Cat In The Hat";
 
-        String upperCaseTitle = "";
+        String upperCaseTitle = bookTitle.toUpperCase();
         // TODO: Convert the book title to upper case and assign it to the lowerCaseTitle variable
-
+        System.out.println(upperCaseTitle);
         assertThat(upperCaseTitle, equalTo("THE CAT IN THE HAT"));
     }
 
@@ -32,9 +33,10 @@ public class WhenWorkingWithStrings {
     public void trimExtraSpaces() {
         String bookTitle = "  The Cat In The Hat    ";
 
-        String trimmedTitle = "";
+        String trimmedTitle = bookTitle.trim();
         // TODO: Trim the spaces before and after the title text
-
+        System.out.println(bookTitle);
+        System.out.println(trimmedTitle);
         assertThat(trimmedTitle, equalTo("The Cat In The Hat"));
     }
 
@@ -43,8 +45,10 @@ public class WhenWorkingWithStrings {
         String bookTitle = "The Cat In The Hat";
 
         int length = 0;
+        length = bookTitle.length();
         // TODO: Find the number of characters in the string
-
+        System.out.println(bookTitle);
+        System.out.println(length);
         assertThat(length, equalTo(18));
     }
 
@@ -52,9 +56,10 @@ public class WhenWorkingWithStrings {
     public void replacingAText() {
         String bookTitle = "The Cat In The Hat";
 
-        String updatedTitle = "";
+        String updatedTitle = bookTitle.replace("Cat", "Dog");
         // TODO: Replace the word "Cat" with "Dog
-
+        System.out.println(bookTitle);
+        System.out.println(updatedTitle);
         assertThat(updatedTitle, equalTo("The Dog In The Hat"));
     }
 }
